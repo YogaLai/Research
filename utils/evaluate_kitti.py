0d65b7730633fbb2d8d9056e5d5ccd8f4db3a3a4
YogaLai <yoga.lai1025@gmail.com>
2022-03-27 23:32:44 +0800

    add epe all,noc,occ metric

--- utils/evaluate_kitti.py
+++ utils/evaluate_kitti.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
 
     pred_disparities = np.load(args.predicted_disp_path)
     for idx, plt_disp in enumerate(pred_disparities):
-        # plt_disp = plt_disp.cpu().data.numpy()
         plt_disp = (plt_disp*256).astype('uint16')
         plt_disp = Image.fromarray(plt_disp)
         if idx % 10 == 0:
@@ -35,8 +34,8 @@ if __name__ == '__main__':
 
     if args.split == 'kitti':
         num_samples = 200
-        
-        gt_disparities = load_gt_disp_kitti(args.gt_path, 'noc')
+        gt_disparities = load_gt_disp_kitti(args.gt_path)
+        gt_disparities_noc = load_gt_disp_kitti(args.gt_path, 'noc')
         gt_depths, pred_depths, pred_disparities_resized = convert_disps_to_depths_kitti(gt_disparities, pred_disparities)
        
 
@@ -71,7 +70,9 @@ if __name__ == '__main__':
     a1      = np.zeros(num_samples, np.float32)
     a2      = np.zeros(num_samples, np.float32)
     a3      = np.zeros(num_samples, np.float32)
-    epe     = np.zeros(num_samples, np.float32)
+    epe_all = np.zeros(num_samples, np.float32)
+    epe_noc = np.zeros(num_samples, np.float32)
+    epe_occ = np.zeros(num_samples, np.float32)
     
     for i in range(num_samples):
         
@@ -104,14 +105,17 @@ if __name__ == '__main__':
 
         if args.split == 'kitti':
             gt_disp = gt_disparities[i]
+            gt_disp_noc = gt_disparities_noc[i]
             mask = gt_disp > 0
+            noc_mask = gt_disp_noc > 0
             pred_disp = pred_disparities_resized[i]
 
-            disp_diff = np.abs(gt_disp[mask] - pred_disp[mask]) # epe
+            disp_diff = np.abs(gt_disp[mask] - pred_disp[mask]) # epe_all
             bad_pixels = np.logical_and(disp_diff >= 3, (disp_diff / gt_disp[mask]) >= 0.05)
             d1_all[i] = 100.0 * bad_pixels.sum() / mask.sum()
 
-        abs_rel[i], sq_rel[i], rms[i], log_rms[i], a1[i], a2[i], a3[i], epe[i] = compute_errors(gt_depth[mask], pred_depth[mask])
+        abs_rel[i], sq_rel[i], rms[i], log_rms[i], a1[i], a2[i], a3[i] = compute_errors(gt_depth[mask], pred_depth[mask])
+        epe_all[i], epe_noc[i], epe_occ[i] = compute_epe(np.abs(gt_disp - pred_disp), mask, noc_mask)
 
-    print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('abs_rel', 'sq_rel', 'rms', 'log_rms', 'd1_all', 'a1', 'a2', 'a3', 'epe'))
-    print("{:10.4f}, {:10.4f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}".format(abs_rel.mean(), sq_rel.mean(), rms.mean(), log_rms.mean(), d1_all.mean(), a1.mean(), a2.mean(), a3.mean(), epe.mean()))
+    print("{:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}, {:>8}".format('abs_rel', 'sq_rel', 'rms', 'log_rms', 'd1_all', 'a1', 'a2', 'a3', 'epe_all', 'epe_noc', 'epe_occ'))
+    print("{:8.4f}, {:8.4f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}, {:8.3f}".format(abs_rel.mean(), sq_rel.mean(), rms.mean(), log_rms.mean(), d1_all.mean(), a1.mean(), a2.mean(), a3.mean(), epe_all.mean(), epe_noc.mean(), epe_occ.mean()))
