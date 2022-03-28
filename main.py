480ac0e7f679bfa36da5d89bad2fb06ab8267836
YogaLai <yoga.lai1025@gmail.com>
2022-03-28 12:40:46 +0800

    modify code to fit PEP8 rules

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from tqdm import tqdm
 import os
 from config import cfg, cfg_from_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    
     parser.add_argument('--model_name',                type=str,   help='model name', default='pwc')
     parser.add_argument('--data_path',                 type=str,   help='path to the data', required=True)
     parser.add_argument('--filenames_file',            type=str,   help='path to the filenames text file', required=True)
@@ -37,6 +37,7 @@ def get_args():
     args = parser.parse_args()
     return args
 
+
 args = get_args()
 writer = SummaryWriter('logs/' + args.exp_name)
 iter = 0
@@ -56,17 +57,17 @@ elif args.model_name == 'dicl':
 
 left_image_1, left_image_2, right_image_1, right_image_2 = get_kitti_cycle_data(args.filenames_file, args.data_path)
 CycleLoader = torch.utils.data.DataLoader(
-         myCycleImageFolder(left_image_1, left_image_2, right_image_1, right_image_2, True, args), 
-         batch_size = args.batch_size, shuffle = True, drop_last = False)
-optimizer = optim.Adam(net.parameters(), lr = args.learning_rate)
+    myCycleImageFolder(left_image_1, left_image_2, right_image_1, right_image_2, True, args),
+    batch_size=args.batch_size, shuffle=True, drop_last=False)
+optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[4, 7, 10, 13], gamma=0.5)
 
 slices = []
 for i in range(4):
-    slices.append(list(range(i*args.batch_size, (i+1)*args.batch_size)))
+    slices.append(list(range(i * args.batch_size, (i + 1) * args.batch_size)))
 
-if torch.cuda.device_count() >=2:
-    net = nn.DataParallel(net) 
+if torch.cuda.device_count() >= 2:
+    net = nn.DataParallel(net)
 
 if args.loadmodel:
     checkpoint = torch.load(args.loadmodel)
@@ -76,12 +77,10 @@ if args.loadmodel:
     optimizer.load_state_dict(checkpoint['optimizer'])
     iter = int(start_epoch * len(CycleLoader.dataset) / args.batch_size) + 1
 
-
-
 for epoch in range(start_epoch, args.num_epochs):
     print("Epoch :", epoch)
-    total_image_loss = 0 
-    total_image_loss_2 = 0 
+    total_image_loss = 0
+    total_image_loss_2 = 0
     total_disp_gradient_loss = 0
     total_disp_gradient_loss_2 = 0
     total_lr_loss = 0
@@ -96,72 +95,72 @@ for epoch in range(start_epoch, args.num_epochs):
 
             former = torch.cat((left_image_2, left_image_1, right_image_1, left_image_1), 0)
             latter = torch.cat((right_image_2, left_image_2, right_image_2, right_image_1), 0)
-                        
+
             left_pyramid = make_pyramid(former, 4)
             right_pyramid = make_pyramid(latter, 4)
 
             model_input = torch.cat((former, latter), 1).cuda()
             model_input_2 = torch.cat((latter, former), 1).cuda()
-            
+
             disp_est_scale = net(model_input)
-            disp_est = [torch.cat((disp_est_scale[i][:,0,:,:].unsqueeze(1) / disp_est_scale[i].shape[3],
-                                disp_est_scale[i][:,1,:,:].unsqueeze(1) / disp_est_scale[i].shape[2]), 1) for i in range(4)]
+            disp_est = [torch.cat((disp_est_scale[i][:, 0, :, :].unsqueeze(1) / disp_est_scale[i].shape[3],
+                                   disp_est_scale[i][:, 1, :, :].unsqueeze(1) / disp_est_scale[i].shape[2]), 1) for i in range(4)]
             disp_est_scale_2 = net(model_input_2)
-            disp_est_2 = [torch.cat((disp_est_scale_2[i][:,0,:,:].unsqueeze(1) / disp_est_scale_2[i].shape[3],
-                                    disp_est_scale_2[i][:,1,:,:].unsqueeze(1) / disp_est_scale_2[i].shape[2]), 1) for i in range(4)]
-            
+            disp_est_2 = [torch.cat((disp_est_scale_2[i][:, 0, :, :].unsqueeze(1) / disp_est_scale_2[i].shape[3],
+                                     disp_est_scale_2[i][:, 1, :, :].unsqueeze(1) / disp_est_scale_2[i].shape[2]), 1) for i in range(4)]
+
             border_mask = [create_border_mask(left_pyramid[i], 0.1) for i in range(4)]
-            
             fw_mask = []
             bw_mask = []
             for i in range(4):
                 fw, bw, diff_fw, diff_bw = get_mask(disp_est_scale[i], disp_est_scale_2[i], border_mask[i])
                 fw += 1e-3
                 bw += 1e-3
-                fw[slices[0]+slices[-1]] = fw[slices[0]+slices[-1]] * 0 + 1
-                bw[slices[0]+slices[-1]] = bw[slices[0]+slices[-1]] * 0 + 1
+                fw[slices[0] + slices[-1]] = fw[slices[0] + slices[-1]] * 0 + 1
+                bw[slices[0] + slices[-1]] = bw[slices[0] + slices[-1]] * 0 + 1
                 fw_detached = fw.clone().detach()
                 bw_detached = bw.clone().detach()
                 fw_mask.append(fw_detached)
                 bw_mask.append(bw_detached)
 
-            
-            #reconstruction from right to left
+            # Reconstruction from right to left
             left_est = [Resample2d()(right_pyramid[i], disp_est_scale[i]) for i in range(4)]
             l1_left = [torch.abs(left_est[i] - left_pyramid[i]) * fw_mask[i] for i in range(4)]
             l1_reconstruction_loss_left = [torch.mean(l1_left[i]) / torch.mean(fw_mask[i]) for i in range(4)]
             ssim_left = [SSIM(left_est[i] * fw_mask[i], left_pyramid[i] * fw_mask[i]) for i in range(4)]
             ssim_loss_left = [torch.mean(ssim_left[i]) / torch.mean(fw_mask[i]) for i in range(4)]
-            image_loss_left  = [args.alpha_image_loss * ssim_loss_left[i] +
-                                (1 - args.alpha_image_loss) * l1_reconstruction_loss_left[i]  for i in range(4)]
+            image_loss_left = [
+                args.alpha_image_loss * ssim_loss_left[i] + (1 - args.alpha_image_loss) * l1_reconstruction_loss_left[i] for i in range(4)
+            ]
             image_loss = sum(image_loss_left)
-            
+
             disp_loss = [cal_grad2_error(disp_est_scale[i] / 20, left_pyramid[i], 1.0) for i in range(4)]
             disp_gradient_loss = sum(disp_loss)
-            
-            #reconstruction from left to right
+
+            # Reconstruction from left to right
             right_est = [Resample2d()(left_pyramid[i], disp_est_scale_2[i]) for i in range(4)]
             l1_right = [torch.abs(right_est[i] - right_pyramid[i]) * bw_mask[i] for i in range(4)]
             l1_reconstruction_loss_right = [torch.mean(l1_right[i]) / torch.mean(bw_mask[i]) for i in range(4)]
             ssim_right = [SSIM(right_est[i] * bw_mask[i], right_pyramid[i] * bw_mask[i]) for i in range(4)]
             ssim_loss_right = [torch.mean(ssim_right[i]) / torch.mean(bw_mask[i]) for i in range(4)]
-            image_loss_right  = [args.alpha_image_loss * ssim_loss_right[i] +
-                                (1 - args.alpha_image_loss) * l1_reconstruction_loss_right[i]  for i in range(4)]
+            image_loss_right = [
+                args.alpha_image_loss * ssim_loss_right[i] + (1 - args.alpha_image_loss) * l1_reconstruction_loss_right[i] for i in range(4)
+            ]
             image_loss_2 = sum(image_loss_right)
-            
+
             disp_loss_2 = [cal_grad2_error(disp_est_scale_2[i] / 20, right_pyramid[i], 1.0) for i in range(4)]
             disp_gradient_loss_2 = sum(disp_loss_2)
-            
-            #LR consistency
+
+            # LR consistency
             right_to_left_disp = [- Resample2d()(disp_est_2[i], disp_est_scale[i]) for i in range(4)]
             left_to_right_disp = [- Resample2d()(disp_est[i], disp_est_scale_2[i]) for i in range(4)]
-            
-            lr_left_loss  = [torch.mean(torch.abs(right_to_left_disp[i][slices[0]+slices[-1]] - disp_est[i][slices[0]+slices[-1]]))  for i in range(4)]
-            lr_right_loss = [torch.mean(torch.abs(left_to_right_disp[i][slices[0]+slices[-1]] - disp_est_2[i][slices[0]+slices[-1]])) for i in range(4)]
+
+            lr_left_loss = [torch.mean(torch.abs(right_to_left_disp[i][slices[0] + slices[-1]] - disp_est[i][slices[0] + slices[-1]])) for i in range(4)]
+            lr_right_loss = [torch.mean(torch.abs(left_to_right_disp[i][slices[0] + slices[-1]] - disp_est_2[i][slices[0] + slices[-1]])) for i in range(4)]
             lr_loss = sum(lr_left_loss + lr_right_loss)
-            
+
             loss = image_loss + image_loss_2 + 10 * (disp_gradient_loss + disp_gradient_loss_2) + args.lr_loss_weight * lr_loss
-            
+
             """
             ##########################################################################################
             #                                                                                        #
@@ -176,7 +175,7 @@ for epoch in range(start_epoch, args.num_epochs):
             #                                                                                        #
             ##########################################################################################
             """
-            
+
             if args.type_of_2warp == 1:
                 mask_4 = [fw_mask[i][[2,3]] for i in range(4)]
                 warp2_est_4 = [Resample2d()(left_est[i][[0,1]], disp_est_scale[i][[2,3]]) for i in range(4)]
@@ -184,7 +183,7 @@ for epoch in range(start_epoch, args.num_epochs):
                 mask_5 = [bw_mask[i][[2,3]] for i in range(4)]
                 warp2_est_5 = [Resample2d()(left_est[i][[6,7]], disp_est_scale_2[i][[2,3]]) for i in range(4)]
                 loss += 0.1 * sum([warp_2(warp2_est_5[i], left_pyramid[i][[0,1]], mask_5[i], args) for i in range(4)])
-                
+
             elif args.type_of_2warp == 2:
                 # mask = [Resample2d()(fw_mask[i][[2,3]], disp_est_scale_2[i][[0,1]]) for i in range(4)]
                 mask = [Resample2d()(fw_mask[i][slices[1]], disp_est_scale_2[i][slices[0]]) for i in range(4)]
@@ -200,19 +199,17 @@ for epoch in range(start_epoch, args.num_epochs):
                 # warp2loss_2 = sum([warp_2(warp2_est_3[i], left_est[i][[6,7]], mask_3[i], args) for i in range(4)])
                 warp2loss_2 = sum([warp_2(warp2_est_3[i], left_est[i][slices[-1]], mask_3[i], args) for i in range(4)])
                 loss += 0.1 * warp2loss_2
-                
+
             elif args.type_of_2warp == 3:
-                mask = [Resample2d()(fw_mask[i][[2,3]], disp_est_scale_2[i][[0,1]]) for i in range(4)]
-                warp2_est = [Resample2d()(left_est[i][[2,3]], disp_est_scale_2[i][[6,7]]) for i in range(4)]
-                loss += 0.1 * sum([warp_2(warp2_est[i], right_pyramid[i][[6,7]], mask[i], args) for i in range(4)])
-                mask_2 = [fw_mask[i][[4,5]] for i in range(4)]
-                warp2_est_2 = [Resample2d()(right_est[i][[0,1]], disp_est_scale[i][[4,5]]) for i in range(4)]
-                loss += 0.1 * sum([warp_2(warp2_est_2[i], right_pyramid[i][[6,7]], mask_2[i], args) for i in range(4)])
-                
-            
+                mask = [Resample2d()(fw_mask[i][[2, 3]], disp_est_scale_2[i][[0, 1]]) for i in range(4)]
+                warp2_est = [Resample2d()(left_est[i][[2, 3]], disp_est_scale_2[i][[6, 7]]) for i in range(4)]
+                loss += 0.1 * sum([warp_2(warp2_est[i], right_pyramid[i][[6, 7]], mask[i], args) for i in range(4)])
+                mask_2 = [fw_mask[i][[4, 5]] for i in range(4)]
+                warp2_est_2 = [Resample2d()(right_est[i][[0, 1]], disp_est_scale[i][[4, 5]]) for i in range(4)]
+                loss += 0.1 * sum([warp_2(warp2_est_2[i], right_pyramid[i][[6, 7]], mask_2[i], args) for i in range(4)])
+
             loss.backward()
             optimizer.step()
-            
 
             writer.add_scalar('iter/rec_loss', image_loss.data, iter)
             writer.add_scalar('iter/rec_loss_2', image_loss_2.data, iter)
@@ -238,11 +235,11 @@ for epoch in range(start_epoch, args.num_epochs):
                 writer.add_images('left_rgb', left_image_1, iter)
                 writer.add_images('right_rgb', right_image_1, iter)
 
-            if (iter+1) % 600 == 0:
+            if (iter + 1) % 600 == 0:
                 state = {'iter': iter, 'epoch': epoch, 'state_dict': net.state_dict(), 'optimizer': optimizer.state_dict(), 'scheduler': scheduler}
                 torch.save(state, "savemodel/" + args.exp_name + "/model_iter" + str(iter))
                 print("The model of iter ", iter, "has been saved.")
-            
+
             iter += 1
             pbar.set_description(
                 f"loss: {loss.item():.5f}"
@@ -251,17 +248,16 @@ for epoch in range(start_epoch, args.num_epochs):
 
     scheduler.step()
 
-    writer.add_scalar('epoch/rec_loss', total_image_loss/len(CycleLoader.dataset), epoch)
-    writer.add_scalar('epoch/rec_loss_2', total_image_loss_2/len(CycleLoader.dataset), epoch)
-    writer.add_scalar('epoch/smooth_loss', total_disp_gradient_loss/len(CycleLoader.dataset), epoch)
-    writer.add_scalar('epoch/smooth_loss_2', total_disp_gradient_loss_2/len(CycleLoader.dataset), epoch)
-    writer.add_scalar('epoch/lr_consistency', total_lr_loss/len(CycleLoader.dataset), epoch)
+    writer.add_scalar('epoch/rec_loss', total_image_loss / len(CycleLoader.dataset), epoch)
+    writer.add_scalar('epoch/rec_loss_2', total_image_loss_2 / len(CycleLoader.dataset), epoch)
+    writer.add_scalar('epoch/smooth_loss', total_disp_gradient_loss / len(CycleLoader.dataset), epoch)
+    writer.add_scalar('epoch/smooth_loss_2', total_disp_gradient_loss_2 / len(CycleLoader.dataset), epoch)
+    writer.add_scalar('epoch/lr_consistency', total_lr_loss / len(CycleLoader.dataset), epoch)
     if args.type_of_2warp > 0:
-        writer.add_scalar('epoch/warp2loss', total_warp2loss/len(CycleLoader.dataset), epoch)
-        writer.add_scalar('epoch/warp2loss_2', total_warp2loss_2/len(CycleLoader.dataset), epoch)
+        writer.add_scalar('epoch/warp2loss', total_warp2loss / len(CycleLoader.dataset), epoch)
+        writer.add_scalar('epoch/warp2loss_2', total_warp2loss_2 / len(CycleLoader.dataset), epoch)
 
     # if epoch % 1 == 0:
     state = {'epoch': epoch, 'state_dict': net.state_dict(), 'optimizer': optimizer.state_dict(), 'scheduler': scheduler}
     torch.save(state, "savemodel/" + args.exp_name + "/model_epoch" + str(epoch))
     print("The model of epoch ", epoch, "has been saved.")
-           
