598d136d98e222968cba312dbc6b0ec326075bae
YogaLai <yoga.lai1025@gmail.com>
2022-04-14 13:47:14 +0800

    fix matchingnet deep bug

--- utils/correltaiton.py
+++ utils/correltaiton.py
@@ -177,7 +177,7 @@ class MatchingNetSmall(nn.Module):
 class MatchingNetDeep(nn.Module):
     # Matching net with 2D conv as mentioned in the paper
     def __init__(self, dcn=False):
-        super(MatchingNetSmall, self).__init__()
+        super(MatchingNetDeep, self).__init__()
         self.match = nn.Sequential(
                         BasicConv(32, 48, kernel_size=3, padding=1, dcn=dcn),
                         BasicConv(48, 96, kernel_size=3, padding=1, stride=2, dcn=dcn),   # down by 1/2
